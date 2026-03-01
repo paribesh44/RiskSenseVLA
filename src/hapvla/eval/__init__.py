a80114583be15347739ecdd20f404e5b28326525
Paribesh Panta <paribeshpanta44@gmail.com>
2026-03-01 13:24:53 +0545

    Add evaluation metrics and visualizations for HAPVLA, including FrameMetrics and SequenceMetrics classes, along with functions for calculating various performance metrics and generating diagnostic plots.

---
+++ src/hapvla/eval/__init__.py
@@ -0,0 +1,17 @@
+from .metrics import (
+    SequenceMetrics,
+    aggregate_sequences,
+    evaluate_sequence,
+    frame_metrics,
+    plot_failure_heatmap,
+    plot_hoi_trajectory,
+)
+
+__all__ = [
+    "SequenceMetrics",
+    "aggregate_sequences",
+    "evaluate_sequence",
+    "frame_metrics",
+    "plot_failure_heatmap",
+    "plot_hoi_trajectory",
+]
